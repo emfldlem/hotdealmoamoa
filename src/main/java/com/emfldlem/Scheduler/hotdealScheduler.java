b76dce001050d89e0b93130c311b43bde6558ed5
swkim <swkim@cyglobal.net>
2020-01-06 22:41:10 +0900

    2020-01-06 / 업데이트

--- src/main/java/com/emfldlem/Scheduler/hotdealScheduler.java
+++ src/main/java/com/emfldlem/Scheduler/hotdealScheduler.java
@@ -11,8 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
+import org.springframework.util.StringUtils;
 
 @Slf4j
 @Component
@@ -27,76 +26,90 @@ public class hotdealScheduler {
     @Autowired
     HotdealService hotdealService;
 
-    @Scheduled(cron = "* */10  *  *  * *")
-    void getRuriDataTEST() throws IOException {
+    //10분마다
+    //@Scheduled(cron = "* */10  *  *  * *")
+    //10초마다
+    @Scheduled(cron = "*/10 *  *  *  * *")
+
+    void getRuriDataTEST() {
         log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 스케줄 시작한다.");
 
-        String URL = "";
-        //for (int i = 1; i <= 2; i++) {
-        URL = ruriwebUrl + "1";
-        Document doc = Jsoup.connect(URL).get();
-        Elements elemList = doc.select(".table_body:not(.notice)");
+        try {
+            String URL = "";
+            URL = ruriwebUrl + "1";
+            Document doc = Jsoup.connect(URL).get();
+            Elements elemList = doc.select(".table_body:not(.notice)");
 
-        for (Element anElem : elemList) {
-            String no = anElem.select(".id").text();
-            int empNo = Integer.parseInt(anElem.select(".id").text());
-            int lastNo = Integer.parseInt(hotdealService.getLastBoardNo("ruriweb"));
+            for (Element anElem : elemList) {
+                if(!StringUtils.isEmpty(anElem.select(".id").text()) ){
+                    String no = anElem.select(".id").text();
+                    int empNo = Integer.parseInt(anElem.select(".id").text());
+                    int lastNo = Integer.parseInt(hotdealService.getLastBoardNo("ruriweb"));
 
-            if (empNo > lastNo) {
-                HotdealEntity hotdealEntity = new HotdealEntity();
-                String subject = anElem.select(".deco").text();
-                String url = anElem.select(".deco").attr("href");
+                    if (empNo > lastNo) {
+                        HotdealEntity hotdealEntity = new HotdealEntity();
+                        String subject = anElem.select(".deco").text();
+                        String url = anElem.select(".deco").attr("href");
 
-                hotdealEntity.setNo(no);
-                hotdealEntity.setBoardSe("ruriweb");
-                hotdealEntity.setSubject(subject);
-                hotdealEntity.setUrl(url);
+                        hotdealEntity.setNo(no);
+                        hotdealEntity.setBoardSe("ruriweb");
+                        hotdealEntity.setSubject(subject);
+                        hotdealEntity.setUrl(url);
 
-                hotdealService.saveHotdealInfo(hotdealEntity);
+                        hotdealService.saveHotdealInfo(hotdealEntity);
+
+                    }
+                }
 
             }
-
         }
-
-        //}
+        catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 
-    @Scheduled(cron = "* */10  *  *  * *")
-    void getPPoData() throws IOException {
-        String URL = ppomppuUrl;
+    //@Scheduled(cron = "* */10  *  *  * *")
+    @Scheduled(cron = "*/15 *  *  *  * *")
+    void getPPoData() {
+        try {
+            String URL = ppomppuUrl;
 
-        URL = URL + 1;
+            URL = URL + 1;
 
-        Document doc = Jsoup.connect(URL).get();
-        Elements elemList_list1 = doc.select(".list1");
-        Elements elemList_list0 = doc.select(".list0");
-        Elements total = new Elements();
+            Document doc = Jsoup.connect(URL).get();
+            Elements elemList_list1 = doc.select(".list1");
+            Elements elemList_list0 = doc.select(".list0");
+            Elements total = new Elements();
 
-        for (int l = 9; l >= 0; l--) {
-            total.add(elemList_list1.get(l));
-            total.add(elemList_list0.get(l));
-        }
-
-        for (Element anElem : total) {
-
-            HotdealEntity hotdealEntity = new HotdealEntity();
-
-            String no = anElem.child(0).text();
-            String subject = anElem.select(".list_title").text();
-            String url = "http://www.ppomppu.co.kr/zboard/" + anElem.select(".list_title").parents().attr("href");
-
-            int empNo = Integer.parseInt(no);
-            int lastNo = Integer.parseInt(hotdealService.getLastBoardNo("ppomppu"));
-
-            if (empNo > lastNo) {
-
-                hotdealEntity.setNo(no);
-                hotdealEntity.setBoardSe("ppomppu");
-                hotdealEntity.setSubject(subject);
-                hotdealEntity.setUrl(url);
-
-                hotdealService.saveHotdealInfo(hotdealEntity);
+            for (int l = 9; l >= 0; l--) {
+                total.add(elemList_list1.get(l));
+                total.add(elemList_list0.get(l));
             }
+
+            for (Element anElem : total) {
+
+                HotdealEntity hotdealEntity = new HotdealEntity();
+
+                String no = anElem.child(0).text();
+                String subject = anElem.select(".list_title").text();
+                String url = "http://www.ppomppu.co.kr/zboard/" + anElem.select(".list_title").parents().attr("href");
+
+                int empNo = Integer.parseInt(no);
+                int lastNo = Integer.parseInt(hotdealService.getLastBoardNo("ppomppu"));
+
+                if (empNo > lastNo) {
+
+                    hotdealEntity.setNo(no);
+                    hotdealEntity.setBoardSe("ppomppu");
+                    hotdealEntity.setSubject(subject);
+                    hotdealEntity.setUrl(url);
+
+                    hotdealService.saveHotdealInfo(hotdealEntity);
+                }
+            }
+        }
+        catch (Exception e) {
+            log.error(e.getMessage());
         }
 
     }
